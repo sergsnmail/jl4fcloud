ace010c0d734ac52c50db0631368f4cba0e9f209
Sergey Pozdeev <sergsnmail@gmail.com>
2021-05-05 16:08:20 +0300

    2. Переработан механизм пакетной передачи файлов. Передача вынесена в отдельный класс. Информация о файле вынесена в отдельный класс.

---
+++ netty-common-obj/src/main/java/message/method/putfile/FileMetadata.java
@@ -0,0 +1,22 @@
+package message.method.putfile;
+
+public class FileMetadata {
+    private String fileName;
+    private String filePath;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
